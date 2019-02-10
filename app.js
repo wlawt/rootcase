01c546d2e7f656ec548062486c630e545a6c7924
tRieMeRalD <will.lao1-05@outlook.com>
2019-02-09 22:40:27 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const keys = require('./config/keys')
+const stripe = require('stripe')(keys.stripeSecretKey);
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+// Initalize app
+const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set static folder 
+app.use(express.static(`${__dirname}/public`));
+
+// Index route 
+app.get('/', (req, res) => {
+    res.render('index', {
+        stripePublishableKey: keys.stripePublishableKey
+    });
+})
+
+// Charge route
+app.post('/charge', (req, res) => {
+    const amount = 1999;
+    
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+    }).then(customer => stripe.charges.create({
+        amount, 
+        description: 'iPhone 7/8 BioCase',
+        currency: 'cad',
+        customer: customer.id
+    })).then(charge => res.render('success'));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+});
+
+
+
