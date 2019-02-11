f473303d491c3a0466c51679e95f8f2f3edd1aee
tRieMeRalD <will.lao1-05@outlook.com>
2019-02-10 23:00:34 -0500

    changed tags website index

--- public/website/index.php
+++ public/website/index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href="website/style.css">
-    <link rel="shortcut icon" href="img/icon.png" type="image/png">
+    <link rel="shortcut icon" href="/img/icon.png" type="image/png">
 
     <!-- Add icon library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
