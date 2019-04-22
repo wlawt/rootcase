459b9b67c26067619d4c850d5b9a3ffb519c0848
unknown <horizon.william14@gmail.com>
2019-04-21 21:40:45 -0400

    Apr 21 9th commit

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const path = require("path");
-const keys = require("../config/keys")(keys.clientCaptcha);
+const keys = require("../config/keys");
 
 // Get contact page
 router.get("/", function(req, res) {
@@ -11,7 +11,7 @@ router.get("/", function(req, res) {
     title: "Rootcase - Contact Us",
     name: "Rootcase - Contact",
     active2: "active",
-    clientCaptcha: keys.clientCaptcha
+    captchaClient: keys.captchaClient
   });
 });
 
